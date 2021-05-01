e755480c38d1ff637288fa888a4ad1841dadc85b
SoulJahN2 <bilgi@alternatik.com>
2021-05-01 15:31:29 +0300

    new

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  sass: true,
+  modules: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['tr-tr', 'en-us'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'tr-tr',
+  },
+};
